191cc66b3d16848a79134e7943826004ded2632c
s1mptom <zikomar@gmail.com>
2012-07-14 23:44:41 +0000

    changed store_url to root_path

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
   def new
     @cart = current_cart
     if @cart.line_items.empty?
-      redirect_to store_url, :notice => "Your cart is empty"
+      redirect_to root_path, :notice => "Your cart is empty"
       return end
     @order = Order.new
   end
